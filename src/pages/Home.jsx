7e654ba57dc93b8ed6f97d4ab57161f4f76d9e4f
Archan Pandya <pandyaarchan17504@gmail.com>
2023-06-12 13:16:03 +0530

    Completed profile section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,9 @@
 import UserResults from "../components/layout/users/UserResults";
+import UserSearch from "../components/layout/users/UserSearch";
 function Home() {
   return (
     <div>
+      <UserSearch />
       <UserResults />
     </div>
   );
